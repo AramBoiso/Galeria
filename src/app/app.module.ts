daf1d4f6574a15f10b396606d8e59c739b3e12d2
Aram <boiso.ska8@gmail.com>
2018-01-04 08:37:56 -0600

    primera version de la Galeria

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule
